fe1f405291a562caec87cddd338dfeec362eba23
whitefir <white_fir@msn.com>
2019-09-23 11:02:55 +0800

    Update SQLstatements.sql

--- SQLstatements.sql
+++ SQLstatements.sql
@@ -31,3 +31,7 @@ FROM Customers
 LEFT JOIN Orders
 ON Customers.Id=Orders.CustomerId
 WHERE Orders.CustomerId IS null
+
+DELETE p1
+FROM Person as p1, Person as p2
+WHERE p1.Email = p2.Email AND p1.Id > p2.Id
